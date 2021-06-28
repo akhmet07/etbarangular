21b992819a621b9b2b014f32d64ae41c0eb112ca
Akhmet <ahmet_07@mail.ru>
2021-06-28 21:35:50 +0600

    2.7 task 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from './button/button.module';
+
 import { ProductcardModule } from './productcard/productcard.module';
 import { ComboboxModule } from './combobox/combobox.module';
 import { IcontooltipModule } from './icontooltip/icontooltip.module';
@@ -14,6 +16,7 @@ import { IcontooltipModule } from './icontooltip/icontooltip.module';
   ],
   imports: [
     BrowserModule,
+    ButtonModule,
     ProductcardModule,
     ComboboxModule,
     IcontooltipModule
